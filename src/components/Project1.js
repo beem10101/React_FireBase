8bf83d1b67ac39b1ce4a58d2c0a32ecefb81a61b
beem10101 <beem25301@gmail.com>
2022-06-25 12:52:50 +0700

    12:52 25/6/22

---
+++ src/components/Project1.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import '../App.css';
+
+
+const Project1 = () => {
+    const [name,setName] = useState("hello");
+    return (
+    <form classname="form">
+        <h1>information</h1> 
+
+
+        <label>Name</label>
+        <input placeholder='fullName' 
+        value={name}
+        onChange={(e)=>setName(e.target.value)}>
+        </input>
+                        
+        <label>{name}</label>
+        <input placeholder='nickname'></input>
+
+        <label>Tel.</label>
+        <input placeholder='tel.'></input>
+
+        <button>submit</button>
+    </form>
+  )
+}
+
+export default Project1
